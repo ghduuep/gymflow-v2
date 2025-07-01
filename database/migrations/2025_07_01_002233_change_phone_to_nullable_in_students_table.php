7b13473e4f5e59e82e224a6e6edf1101597211e3
Guilherme Lima <ghduep@outlook.com>
2025-07-01 07:06:42 -0300

    removing country from address and changing nullable on phone and email

---
+++ database/migrations/2025_07_01_002233_change_phone_to_nullable_in_students_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('phone', 9)->nullable()->change();
+            $table->string('email')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+             $table->string('phone', 9)->nullable(false)->change();
+             $table->string('email')->nullable()->change();
+        });
+    }
+};
